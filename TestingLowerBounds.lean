b0dbfe5067a6831e7f3ee35ee1487dbf1d7c5e46
RemyDegenne <Remydegenne@gmail.com>
2024-03-13 16:14:33 +0100

    fix lib imports

--- TestingLowerBounds.lean
+++ TestingLowerBounds.lean
@@ -1,3 +1,3 @@
 -- This module serves as the root of the `TestingLowerBounds` library.
 -- Import modules here that should be built as part of the library.
-import TestingLowerBounds.Basic
+import TestingLowerBounds.FDiv
