7ed21a28121a1ea5702d5e604d0d5794aaa1abb2
RemyDegenne <Remydegenne@gmail.com>
2024-03-14 13:44:14 +0100

    fix kl definition

--- TestingLowerBounds/KullbackLeibler.lean
+++ TestingLowerBounds/KullbackLeibler.lean
@@ -64,8 +64,12 @@ lemma todo_div [SigmaFinite μ] [SigmaFinite ν] (hμν : μ ≪ ν) :
 
 end move_this
 
-/-- Kullback-Leibler divergence between two measures, real-valued version. -/
-noncomputable def klReal (μ ν : Measure α) : ℝ := ∫ x, llr μ ν x ∂μ
+open Classical in
+/-- Kullback-Leibler divergence between two measures, real-valued version.
+
+We test `μ ≪ ν` to ensure that this definition is 0 whenever the informal KL should be ∞. -/
+noncomputable def klReal (μ ν : Measure α) : ℝ :=
+  if μ ≪ ν then ∫ x, llr μ ν x ∂μ else 0
 
 open Classical in
 /-- Kullback-Leibler divergence between two measures. -/
@@ -73,9 +77,16 @@ noncomputable
 def kl (μ ν : Measure α) : ℝ≥0∞ :=
   if μ ≪ ν ∧ Integrable (llr μ ν) μ then ENNReal.ofReal (∫ x, llr μ ν x ∂μ) else ∞
 
+lemma klReal_of_ac (h : μ ≪ ν) : klReal μ ν = ∫ x, llr μ ν x ∂μ := if_pos h
+
+@[simp]
+lemma klReal_of_not_ac (h : ¬ μ ≪ ν) : klReal μ ν = 0 := if_neg h
+
 @[simp]
 lemma klReal_of_not_integrable (h : ¬ Integrable (llr μ ν) μ) : klReal μ ν = 0 := by
-  rw [klReal, integral_undef h]
+  by_cases h_ac : μ ≪ ν
+  · rw [klReal_of_ac h_ac, integral_undef h]
+  · exact if_neg h_ac
 
 section llr_and_lrf
 
@@ -84,9 +95,10 @@ lemma integrable_rnDeriv_mul_log [IsFiniteMeasure μ] [IsProbabilityMeasure ν]
     Integrable (fun x ↦ (μ.rnDeriv ν x).toReal * log (μ.rnDeriv ν x).toReal) ν :=
   integrable_rnDeriv_smul hμν h_int
 
-lemma klReal_eq_fDivReal_mul_log [SigmaFinite μ] [Measure.HaveLebesgueDecomposition μ ν] (hμν : μ ≪ ν) :
+lemma klReal_eq_fDivReal_mul_log [SigmaFinite μ] [Measure.HaveLebesgueDecomposition μ ν]
+    (hμν : μ ≪ ν) :
     klReal μ ν = fDivReal (fun x ↦ x * log x) μ ν := by
-  simp_rw [klReal, llr, fDivReal]
+  simp_rw [klReal_of_ac hμν, llr, fDivReal]
   conv_lhs =>
     rw [← Measure.withDensity_rnDeriv_eq _ _ hμν]
     conv in (Measure.rnDeriv (ν.withDensity (μ.rnDeriv ν)) ν) =>
@@ -130,43 +142,46 @@ lemma klReal_ge_mul_log [IsFiniteMeasure μ] [IsFiniteMeasure ν]
     · exact measure_ne_top _ _
   have h := klReal_ge_mul_log' (?_ : μ ≪ ν') ?_
   rotate_left
-  · refine Measure.AbsolutelyContinuous.trans hμν ?_
-    refine Measure.absolutelyContinuous_of_le_smul (c := ν Set.univ) ?_
-    rw [← smul_assoc, smul_eq_mul, ENNReal.mul_inv_cancel, one_smul]
-    · simp [hν]
-    · exact measure_ne_top _ _
+  · refine Measure.AbsolutelyContinuous.trans hμν (Measure.absolutelyContinuous_smul ?_)
+    simp [measure_ne_top ν]
   · rw [integrable_congr (llr_smul_right hμν (ν Set.univ)⁻¹ _ _)]
     rotate_left
     · simp [measure_ne_top ν _]
     · simp [hν]
     exact h_int.sub (integrable_const _)
-  rw [klReal, integral_congr_ae (llr_smul_right hμν (ν Set.univ)⁻¹ _ _)] at h
+  rw [klReal_of_ac, integral_congr_ae (llr_smul_right hμν (ν Set.univ)⁻¹ _ _)] at h
   rotate_left
   · simp [measure_ne_top ν _]
   · simp [hν]
+  · refine hμν.trans (Measure.absolutelyContinuous_smul ?_)
+    simp [measure_ne_top ν]
   rw [integral_sub h_int (integrable_const _), integral_const, smul_eq_mul, le_sub_iff_add_le,
     ENNReal.toReal_inv, log_inv, mul_neg, ← sub_eq_add_neg] at h
-  rwa [log_div, mul_sub]
+  rwa [klReal_of_ac hμν, log_div, mul_sub]
   · rw [ENNReal.toReal_ne_zero]
     simp [hμ, measure_ne_top μ]
   · rw [ENNReal.toReal_ne_zero]
     simp [hν, measure_ne_top ν]
 
-lemma klReal_nonneg [IsProbabilityMeasure μ] [IsProbabilityMeasure ν] (hμν : μ ≪ ν) :
+lemma klReal_nonneg (μ ν : Measure α) [IsProbabilityMeasure μ] [IsProbabilityMeasure ν] :
     0 ≤ klReal μ ν := by
+  by_cases hμν : μ ≪ ν
+  swap; · simp [hμν]
   by_cases h_int : Integrable (llr μ ν) μ
   · rw [klReal_eq_fDivReal_mul_log hμν]
     exact fDivReal_nonneg Real.convexOn_mul_log Real.continuous_mul_log.continuousOn
       (by simp) (integrable_rnDeriv_mul_log hμν h_int) hμν
-  · rw [klReal, integral_undef h_int]
+  · rw [klReal_of_ac hμν, integral_undef h_int]
 
 end klReal_nonneg
 
 lemma klReal_tilted_right [IsProbabilityMeasure μ] [SigmaFinite ν]
     (hμν : μ ≪ ν) (h_int : Integrable (llr μ ν) μ)
     {f : α → ℝ} (hfμ : Integrable f μ) (hf : Integrable (fun x ↦ exp (f x)) ν) :
-    klReal μ (ν.tilted f) = klReal μ ν - ∫ x, f x ∂μ + log (∫ x, exp (f x) ∂ν) :=
-  integral_llr_tilted_right hμν hfμ hf h_int
+    klReal μ (ν.tilted f) = klReal μ ν - ∫ x, f x ∂μ + log (∫ x, exp (f x) ∂ν) := by
+  rw [klReal_of_ac, klReal_of_ac hμν]
+  · exact integral_llr_tilted_right hμν hfμ hf h_int
+  · exact hμν.trans (absolutelyContinuous_tilted hf)
 
 lemma integral_sub_log_integral_exp_le_klReal [IsProbabilityMeasure μ] [IsProbabilityMeasure ν]
     (hμν : μ ≪ ν) (h_int : Integrable (llr μ ν) μ)
@@ -178,7 +193,7 @@ lemma integral_sub_log_integral_exp_le_klReal [IsProbabilityMeasure μ] [IsProba
   rw [← this]
   simp only [tsub_le_iff_right, le_add_iff_nonneg_right]
   have : IsProbabilityMeasure (Measure.tilted ν f) := isProbabilityMeasure_tilted hf
-  exact klReal_nonneg (hμν.trans (absolutelyContinuous_tilted hf))
+  exact klReal_nonneg _ _
 
 /-- One side of the Donsker-Varadhan variational formula for the Kullback-Leibler divergence.
 See `klReal_eq_ciSup` for the equality. -/
@@ -194,8 +209,8 @@ lemma ciSup_le_klReal [IsProbabilityMeasure μ] [IsProbabilityMeasure ν]
     · simp only [hf, ciSup_unique]
       exact integral_sub_log_integral_exp_le_klReal hμν h_int f hfμ hf
     · simp [hf, csSup_empty]
-      exact klReal_nonneg hμν
+      exact klReal_nonneg _ _
   · simp only [hfμ, csSup_empty, iSup_of_isEmpty]
-    exact klReal_nonneg hμν
+    exact klReal_nonneg _ _
 
 end ProbabilityTheory
