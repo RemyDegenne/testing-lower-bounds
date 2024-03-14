6faf2dcc2ec8fc5039175cafaba5bd2396b1ba8c
RemyDegenne <Remydegenne@gmail.com>
2024-03-14 09:00:22 +0100

    integrability for conditional fDiv

--- TestingLowerBounds/FDiv.lean
+++ TestingLowerBounds/FDiv.lean
@@ -51,7 +51,8 @@ open scoped ENNReal
 
 namespace ProbabilityTheory
 
-variable {α : Type*} {mα : MeasurableSpace α} {μ ν : Measure α} {f : ℝ → ℝ}
+variable {α β : Type*} {mα : MeasurableSpace α} {mβ : MeasurableSpace β}
+  {μ ν : Measure α} {κ η : kernel α β} {f : ℝ → ℝ}
 
 /-- f-Divergence of two measures, real-valued. -/
 noncomputable
@@ -89,5 +90,110 @@ lemma fDivReal_self (hf_one : f 1 = 0) (μ : Measure α) [SigmaFinite μ] : fDiv
     rfl
   rw [fDivReal, integral_congr_ae h]
   simp
+
+section Conditional
+
+noncomputable
+def condFDivReal (f : ℝ → ℝ) (κ η : kernel α β) (μ : Measure α) : ℝ :=
+  μ[fun x ↦ fDivReal f (κ x) (η x)]
+
+variable [MeasurableSpace.CountablyGenerated β]
+
+section Integrable
+
+/-! We show that the integrability of the functions used in `fDivReal f (μ ⊗ₘ κ) (μ ⊗ₘ η)`
+and in `condFDivReal f κ η μ` are equivalent, when `f` is bounded from below and `μ` is finite.
+
+TODO. -/
+
+lemma integrable_f_rnDeriv_of_integrable_compProd [IsFiniteMeasure μ] [IsFiniteKernel κ]
+    [IsFiniteKernel η] (hf : StronglyMeasurable f)
+    (hf_int : Integrable (fun x ↦ f ((μ ⊗ₘ κ).rnDeriv (μ ⊗ₘ η) x).toReal) (μ ⊗ₘ η)) :
+    ∀ᵐ a ∂μ, Integrable (fun x ↦ f ((κ a).rnDeriv (η a) x).toReal) (η a) := by
+  have h := hf_int.integral_compProd
+  simp only [kernel.prodMkLeft_apply, kernel.const_apply] at h
+  rw [Measure.integrable_compProd_iff] at hf_int
+  swap
+  · exact (hf.comp_measurable (Measure.measurable_rnDeriv _ _).ennreal_toReal).aestronglyMeasurable
+  have h := Measure.ae_ae_of_ae_compProd (kernel.rnDeriv_measure_compProd_right μ κ η)
+  filter_upwards [h, hf_int.1] with a ha1 ha2
+  refine (integrable_congr ?_).mp ha2
+  filter_upwards [ha1, kernel.rnDeriv_eq_rnDeriv_measure κ η a] with b hb h_eq
+  rw [hb, h_eq]
+
+lemma integrable_fDivReal_of_integrable_compProd [IsFiniteMeasure μ] [IsFiniteKernel κ]
+    [IsFiniteKernel η]
+    (hf_int : Integrable (fun x ↦ f ((μ ⊗ₘ κ).rnDeriv (μ ⊗ₘ η) x).toReal) (μ ⊗ₘ η)) :
+    Integrable (fun x ↦ fDivReal f (κ x) (η x)) μ := by
+  have h := hf_int.integral_compProd
+  simp only [kernel.prodMkLeft_apply, kernel.const_apply] at h
+  have h_eq_compProd := Measure.ae_ae_of_ae_compProd (kernel.rnDeriv_measure_compProd_right μ κ η)
+  refine (integrable_congr ?_).mp h
+  filter_upwards [h_eq_compProd] with a ha
+  rw [fDivReal]
+  refine integral_congr_ae ?_
+  filter_upwards [ha, kernel.rnDeriv_eq_rnDeriv_measure κ η a] with b hb h_eq
+  rw [hb, h_eq]
+
+lemma integrable_f_rnDeriv_compProd_iff [IsFiniteMeasure μ] [IsFiniteKernel κ]
+    [IsFiniteKernel η] (hf : StronglyMeasurable f) (hbdd : BddBelow (f '' (Set.Ici 0))) :
+    Integrable (fun x ↦ f ((μ ⊗ₘ κ).rnDeriv (μ ⊗ₘ η) x).toReal) (μ ⊗ₘ η)
+      ↔ (∀ᵐ a ∂μ, Integrable (fun x ↦ f ((κ a).rnDeriv (η a) x).toReal) (η a))
+        ∧ Integrable (fun x ↦ fDivReal f (κ x) (η x)) μ := by
+  have h_eq_compProd := Measure.ae_ae_of_ae_compProd (kernel.rnDeriv_measure_compProd_right μ κ η)
+  have h_ae_eq : ∀ᵐ a ∂μ, (fun y ↦ f ((∂μ ⊗ₘ κ/∂μ ⊗ₘ η) (a, y)).toReal)
+      =ᵐ[η a] fun x ↦ f ((∂κ a/∂η a) x).toReal := by
+    filter_upwards [h_eq_compProd] with a ha
+    filter_upwards [ha, kernel.rnDeriv_eq_rnDeriv_measure κ η a] with b hb h_eq
+    rw [hb, h_eq]
+  refine ⟨fun h ↦ ?_, fun ⟨h1, h2⟩ ↦ ?_⟩
+  · have h_int := h.integral_compProd
+    simp only [kernel.prodMkLeft_apply, kernel.const_apply] at h_int
+    rw [Measure.integrable_compProd_iff] at h
+    swap
+    · exact (hf.comp_measurable
+        (Measure.measurable_rnDeriv _ _).ennreal_toReal).aestronglyMeasurable
+    constructor
+    · filter_upwards [h.1, h_ae_eq] with a ha1 ha2
+      exact (integrable_congr ha2).mp ha1
+    · refine (integrable_congr ?_).mp h_int
+      filter_upwards [h_ae_eq] with a ha
+      rw [fDivReal]
+      exact integral_congr_ae ha
+  · rw [Measure.integrable_compProd_iff]
+    swap
+    · exact (hf.comp_measurable
+        (Measure.measurable_rnDeriv _ _).ennreal_toReal).aestronglyMeasurable
+    constructor
+    · filter_upwards [h1, h_ae_eq] with a ha1 ha2
+      exact (integrable_congr ha2).mpr ha1
+    · sorry
+
+end Integrable
+
+lemma fDivReal_compProd_left (μ : Measure α) [IsFiniteMeasure μ]
+    (κ η : kernel α β) [IsFiniteKernel κ] [IsFiniteKernel η]
+    (hf : StronglyMeasurable f) (hbdd : BddBelow (f '' (Set.Ici 0))) :
+    fDivReal f (μ ⊗ₘ κ) (μ ⊗ₘ η) = condFDivReal f κ η μ := by
+  by_cases hf_int : Integrable (fun x ↦ f ((μ ⊗ₘ κ).rnDeriv (μ ⊗ₘ η) x).toReal) (μ ⊗ₘ η)
+  swap; · sorry
+  rw [condFDivReal, fDivReal]
+  have h_eq : ∀ a, (fun b ↦ f (kernel.rnDeriv κ η a b).toReal)
+      =ᵐ[η a] fun x ↦ f ((∂κ a/∂η a) x).toReal := by
+    intro a
+    filter_upwards [kernel.rnDeriv_eq_rnDeriv_measure κ η a] with b hb
+    rw [hb]
+  rw [Measure.integral_compProd hf_int]
+  have h : ∀ᵐ a ∂μ, ∫ b, f ((∂μ ⊗ₘ κ/∂μ ⊗ₘ η) (a, b)).toReal ∂(η a)
+      = ∫ b, f ((∂(κ a)/∂(η a)) b).toReal ∂(η a) := by
+    filter_upwards [Measure.ae_ae_of_ae_compProd <| kernel.rnDeriv_measure_compProd_right μ κ η]
+      with a ha
+    refine integral_congr_ae ?_
+    filter_upwards [ha, h_eq a] with b hb1 hb2
+    rw [hb1, hb2]
+  rw [integral_congr_ae h]
+  rfl
+
+end Conditional
 
 end ProbabilityTheory
