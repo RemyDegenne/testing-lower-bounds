a136846fd4311feea84b371433261ddf0dee928a
Rémy Degenne <remydegenne@gmail.com>
2024-03-18 13:25:43 +0100

    split fdiv file

--- TestingLowerBounds.lean
+++ TestingLowerBounds.lean
@@ -1,6 +1,7 @@
 -- This module serves as the root of the `TestingLowerBounds` library.
 -- Import modules here that should be built as part of the library.
 import TestingLowerBounds.FDiv
+import TestingLowerBounds.CondFDiv
 import TestingLowerBounds.Hellinger
 import TestingLowerBounds.KullbackLeibler
 import TestingLowerBounds.Renyi
