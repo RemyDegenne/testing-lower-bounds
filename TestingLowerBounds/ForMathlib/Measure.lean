0da82c3c74f3658164745b4683c1d3c9e8063ad7
RemyDegenne <Remydegenne@gmail.com>
2024-05-01 11:54:53 +0200

    spaces

--- TestingLowerBounds/ForMathlib/Measure.lean
+++ TestingLowerBounds/ForMathlib/Measure.lean
@@ -6,23 +6,20 @@ namespace MeasureTheory
 variable {α β : Type*} {m mα : MeasurableSpace α} {mβ : MeasurableSpace β} {μ ν : Measure α}
 
 @[simp]
-lemma Measure.fst_map_swap (μ : Measure (α × β)) :
-    (μ.map Prod.swap).fst = μ.snd := by
-    ext s hs
-    rw [Measure.fst, Measure.map_map measurable_fst measurable_swap, Measure.snd_apply hs,
-      Measure.map_apply (measurable_fst.comp measurable_swap) hs]
-    congr
+lemma Measure.fst_map_swap (μ : Measure (α × β)) : (μ.map Prod.swap).fst = μ.snd := by
+  ext s hs
+  rw [Measure.fst, Measure.map_map measurable_fst measurable_swap, Measure.snd_apply hs,
+    Measure.map_apply (measurable_fst.comp measurable_swap) hs]
+  congr
 
 @[simp]
-lemma Measure.snd_map_swap (μ : Measure (α × β)) :
-    (μ.map Prod.swap).snd = μ.fst := by
-    ext s hs
-    rw [Measure.snd, Measure.map_map measurable_snd measurable_swap, Measure.fst_apply hs,
-      Measure.map_apply (measurable_snd.comp measurable_swap) hs]
-    congr
+lemma Measure.snd_map_swap (μ : Measure (α × β)) : (μ.map Prod.swap).snd = μ.fst := by
+  ext s hs
+  rw [Measure.snd, Measure.map_map measurable_snd measurable_swap, Measure.fst_apply hs,
+    Measure.map_apply (measurable_snd.comp measurable_swap) hs]
+  congr
 
-lemma Measure.AbsolutelyContinuous.add_left {μ₁ μ₂ ν : Measure α}
-    (h₁ : μ₁ ≪ ν) (h₂ : μ₂ ≪ ν) :
+lemma Measure.AbsolutelyContinuous.add_left {μ₁ μ₂ ν : Measure α} (h₁ : μ₁ ≪ ν) (h₂ : μ₂ ≪ ν) :
     μ₁ + μ₂ ≪ ν := Measure.AbsolutelyContinuous.add_left_iff.mpr ⟨h₁, h₂⟩
 
 lemma Measure.trim_add (μ ν : Measure α) (hm : m ≤ mα) :
