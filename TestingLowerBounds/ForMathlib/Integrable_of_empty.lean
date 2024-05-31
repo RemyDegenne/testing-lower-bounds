640ef57c5ac3421a50e23603e275e2f0facbcc53
Lorenzo Luccioli <lorenzoluccioli@gmail.com>
2024-05-31 14:29:26 +0200

    apply suggestions

--- TestingLowerBounds/ForMathlib/Integrable_of_empty.lean
+++ TestingLowerBounds/ForMathlib/Integrable_of_empty.lean
@@ -6,22 +6,24 @@ open Real MeasureTheory Filter MeasurableSpace
 open scoped ENNReal NNReal Topology BigOperators
 
 --TODO: put this in mathlib, maybe just after:
-#check Integrable.of_finite
+-- #check Integrable.of_finite
+
+namespace MeasureTheory
 
 @[simp]
-lemma Integrable.of_empty {α β : Type*} [MeasurableSpace α] [NormedAddCommGroup β] [IsEmpty α]
-    (f : α → β) (μ : Measure α) : Integrable f μ := Integrable.of_finite μ f
+lemma Integrable.of_isEmpty {α β : Type*} [MeasurableSpace α] [NormedAddCommGroup β]
+    [IsEmpty α] (f : α → β) (μ : Measure α) : Integrable f μ := Integrable.of_finite μ f
 
 @[simp]
-lemma Integrable.of_empty_codomain {α β : Type*} [MeasurableSpace α] [NormedAddCommGroup β] [IsEmpty β]
-    {f : α → β} {μ : Measure α} : Integrable f μ := by
+lemma Integrable.of_isEmpty_codomain {α β : Type*} [MeasurableSpace α] [NormedAddCommGroup β]
+    [IsEmpty β] (f : α → β) (μ : Measure α) : Integrable f μ :=
   have : IsEmpty α := f.isEmpty
-  exact Integrable.of_empty f μ
+  Integrable.of_isEmpty f μ
 
 --TODO: put this in mathlib, maybe just after:
-#check integral_eq_zero_of_ae
+-- #check integral_eq_zero_of_ae
 
 @[simp]
-lemma MeasureTheory.integral_of_empty {α β : Type*} [MeasurableSpace α] [NormedAddCommGroup β]
-    [NormedSpace ℝ β] [IsEmpty α] (f : α → β) (μ : Measure α) : (∫ x, f x ∂μ) = 0 :=
+lemma integral_of_isEmpty {α β : Type*} [MeasurableSpace α] [NormedAddCommGroup β]
+    [NormedSpace ℝ β] [IsEmpty α] (f : α → β) (μ : Measure α) : ∫ x, f x ∂μ = 0 :=
   integral_eq_zero_of_ae <| eventually_of_forall (IsEmpty.forall_iff.mpr True.intro)
