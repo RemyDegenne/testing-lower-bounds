6f63e95139ad8f9a8cabe3130be85a8a2bfa848a
RemyDegenne <Remydegenne@gmail.com>
2024-09-04 22:49:25 +0200

    lake exe mk_all

--- TestingLowerBounds.lean
+++ TestingLowerBounds.lean
@@ -1,3 +1,4 @@
+import TestingLowerBounds.CompProd
 import TestingLowerBounds.Convex
 import TestingLowerBounds.CurvatureMeasure
 import TestingLowerBounds.DerivAtTop
@@ -18,6 +19,7 @@ import TestingLowerBounds.FDiv.Trim
 import TestingLowerBounds.ForMathlib.ByParts
 import TestingLowerBounds.ForMathlib.CountableOrCountablyGenerated
 import TestingLowerBounds.ForMathlib.EReal
+import TestingLowerBounds.ForMathlib.GiryMonad
 import TestingLowerBounds.ForMathlib.Integrable_of_empty
 import TestingLowerBounds.ForMathlib.KernelFstSnd
 import TestingLowerBounds.ForMathlib.LeftRightDeriv
@@ -27,6 +29,8 @@ import TestingLowerBounds.ForMathlib.RadonNikodym
 import TestingLowerBounds.ForMathlib.RnDeriv
 import TestingLowerBounds.Kernel.Basic
 import TestingLowerBounds.Kernel.BayesInv
+import TestingLowerBounds.Kernel.Deterministic
+import TestingLowerBounds.Kernel.DeterministicComp
 import TestingLowerBounds.Kernel.ParallelComp
 import TestingLowerBounds.MeasureCompProd
 import TestingLowerBounds.SqHellinger
