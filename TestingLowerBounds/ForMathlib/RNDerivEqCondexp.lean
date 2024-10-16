307acf19d18461c301b21b2d659848b4d956ca4f
RemyDegenne <Remydegenne@gmail.com>
2024-10-16 18:12:50 +0200

    golf rnDeriv_compProd, following the blueprint

--- TestingLowerBounds/ForMathlib/RNDerivEqCondexp.lean
+++ TestingLowerBounds/ForMathlib/RNDerivEqCondexp.lean
@@ -51,50 +51,13 @@ theorem _root_.Measurable.setLIntegral_kernel_prod_right' [IsSFiniteKernel κ]
     Measurable fun a => ∫⁻ b in s, f (a, b) ∂κ a := by
   simp_rw [← Kernel.lintegral_restrict κ hs]; exact hf.lintegral_kernel_prod_right'
 
-lemma rnDeriv_compProd [IsFiniteMeasure μ] [IsFiniteMeasure ν]
-    (hμν : μ ≪ ν) [IsFiniteKernel κ] [IsFiniteKernel η] (h_ac : μ ⊗ₘ κ ≪ μ ⊗ₘ η) :
+lemma rnDeriv_compProd [IsFiniteMeasure μ]
+    [IsFiniteKernel κ] [IsFiniteKernel η] (h_ac : μ ⊗ₘ κ ≪ μ ⊗ₘ η)
+    (ν : Measure α) [IsFiniteMeasure ν] :
     (fun p ↦ μ.rnDeriv ν p.1 * (μ ⊗ₘ κ).rnDeriv (μ ⊗ₘ η) p)
       =ᵐ[ν ⊗ₘ η] (μ ⊗ₘ κ).rnDeriv (ν ⊗ₘ η) := by
-  have h_ac1 : μ ⊗ₘ κ ≪ ν ⊗ₘ η := Measure.absolutelyContinuous_compProd_of_compProd hμν h_ac
-  have h_meas : Measurable fun p ↦ (∂μ/∂ν) p.1 * (∂μ ⊗ₘ κ/∂μ ⊗ₘ η) p :=
-    ((Measure.measurable_rnDeriv _ _).comp measurable_fst).mul (Measure.measurable_rnDeriv _ _)
-  have h_eq t₁ t₂ : MeasurableSet t₁ → MeasurableSet t₂ →
-      ∫⁻ p in t₁ ×ˢ t₂, (∂μ/∂ν) p.1 * (∂μ ⊗ₘ κ/∂μ ⊗ₘ η) p ∂ν ⊗ₘ η
-        = ∫⁻ p in t₁ ×ˢ t₂, (∂μ ⊗ₘ κ/∂ν ⊗ₘ η) p ∂ν ⊗ₘ η := by
-    intro ht₁ ht₂
-    rw [Measure.setLIntegral_compProd h_meas ht₁ ht₂]
-    simp only
-    have : ∀ a, ∫⁻ b in t₂, (∂μ/∂ν) a * (∂μ ⊗ₘ κ/∂μ ⊗ₘ η) (a, b) ∂η a
-        = (∂μ/∂ν) a * ∫⁻ b in t₂, (∂μ ⊗ₘ κ/∂μ ⊗ₘ η) (a, b) ∂η a := fun a ↦ by
-      rw [lintegral_const_mul]
-      exact (Measure.measurable_rnDeriv _ _).comp measurable_prod_mk_left
-    simp_rw [this]
-    rw [setLIntegral_rnDeriv_mul hμν _ ht₁]
-    swap
-    · exact ((Measure.measurable_rnDeriv _ _).setLIntegral_kernel_prod_right' ht₂).aemeasurable
-    rw [← Measure.setLIntegral_compProd (Measure.measurable_rnDeriv _ _) ht₁ ht₂,
-      Measure.setLIntegral_rnDeriv h_ac1, Measure.setLIntegral_rnDeriv h_ac]
-  refine ae_eq_of_forall_setLIntegral_eq_of_sigmaFinite h_meas ?_ ?_
-  · exact Measure.measurable_rnDeriv _ _
-  · intro s hs _
-    apply induction_on_inter generateFrom_prod.symm isPiSystem_prod _ _ _ _ hs
-    · simp
-    · rintro _ ⟨t₁, ht₁, t₂, ht₂, rfl⟩
-      exact h_eq t₁ t₂ ht₁ ht₂
-    · intro t ht ht_eq
-      have h_ne_top : ∫⁻ p in t, (∂μ ⊗ₘ κ/∂ν ⊗ₘ η) p ∂ν ⊗ₘ η ≠ ⊤ := by
-        rw [Measure.setLIntegral_rnDeriv]
-        · exact measure_ne_top _ _
-        · exact h_ac1
-      rw [setLintegral_compl ht h_ne_top, setLintegral_compl ht, ht_eq]
-      swap; · rw [ht_eq]; exact h_ne_top
-      congr 1
-      have h := h_eq .univ .univ .univ .univ
-      simp only [univ_prod_univ, Measure.restrict_univ] at h
-      exact h
-    · intro f' hf_disj hf_meas hf_eq
-      rw [lintegral_iUnion hf_meas hf_disj, lintegral_iUnion hf_meas hf_disj]
-      congr with i
-      exact hf_eq i
+  refine Filter.EventuallyEq.trans ?_ (Measure.rnDeriv_mul_rnDeriv h_ac)
+  filter_upwards [Kernel.rnDeriv_measure_compProd_left μ ν η] with p hp
+  rw [Pi.mul_apply, hp, mul_comm]
 
 end ProbabilityTheory
