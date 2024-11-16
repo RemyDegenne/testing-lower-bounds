7fdd3bb9f16dae85b2f76c9575706e356532d3bd
RemyDegenne <Remydegenne@gmail.com>
2024-11-16 11:31:48 +0100

    work on rightDerivStieltjes

--- TestingLowerBounds/FDiv/DivFunction.lean
+++ TestingLowerBounds/FDiv/DivFunction.lean
@@ -476,27 +476,105 @@ lemma rightDeriv_mono (f : DivFunction) {x y : ℝ} (hxy : x ≤ y)
     rightDeriv f.realFun x ≤ rightDeriv f.realFun y := by
   sorry
 
--- lemma continuousWithinAt_rightDeriv (f : DivFunction) {x : ℝ}
---     (hx : f.xmin < ENNReal.ofReal x) (hx' : ENNReal.ofReal x < f.xmax) :
---     ContinuousWithinAt (rightDeriv f.realFun) (Ici x) x := by
---   sorry
+lemma continuousWithinAt_rightDeriv (f : DivFunction) {x : ℝ}
+    (hx : f.xmin < ENNReal.ofReal x) (hx' : ENNReal.ofReal x < f.xmax) :
+    ContinuousWithinAt (rightDeriv f.realFun) (Ici x) x := by
+  sorry
+
+lemma rightLim_congr {α β : Type*} [LinearOrder α] [TopologicalSpace α] [OrderTopology α]
+    [TopologicalSpace β] [T2Space β] {f g : α → β}
+    {a : α} (h_ne_bot : 𝓝[>] a ≠ ⊥) {y : β} (h_tendsto : Tendsto f (𝓝[>] a) (𝓝 y))
+    (h : f =ᶠ[𝓝[>] a] g) :
+    Function.rightLim f a = Function.rightLim g a := by
+  rw [rightLim_eq_of_tendsto h_ne_bot h_tendsto,
+    rightLim_eq_of_tendsto h_ne_bot ((tendsto_congr' h).mp h_tendsto)]
+
+noncomputable def rightDerivFun (f : DivFunction) : ℝ → EReal :=
+  fun x ↦
+    if x < f.xmin.toReal then ⊥
+    else if f.xmax ≤ ENNReal.ofReal x then ⊤
+    else Function.rightLim (fun y ↦ (rightDeriv f.realFun y : EReal)) x
+
+lemma monotone_rightDerivFun (f : DivFunction) : Monotone f.rightDerivFun := by
+  intro x y hxy
+  rcases lt_or_le x f.xmin.toReal with hx_lt_min | hx_ge_min
+  · simp [rightDerivFun, hx_lt_min]
+  rcases le_or_lt f.xmax (ENNReal.ofReal y) with hy_ge_max | hy_lt_max
+  · simp only [rightDerivFun, not_lt.mpr (hx_ge_min.trans hxy), ↓reduceIte, hy_ge_max, le_top]
+  simp only [rightDerivFun, not_lt.mpr hx_ge_min, ↓reduceIte, not_le.mpr hy_lt_max,
+    not_lt.mpr (hx_ge_min.trans hxy)]
+  rw [if_neg]
+  swap
+  · refine not_le.mpr (lt_of_le_of_lt ?_ hy_lt_max)
+    rwa [ENNReal.ofReal_le_ofReal_iff]
+    exact ENNReal.toReal_nonneg.trans (hx_ge_min.trans hxy)
+  sorry
+
+lemma rightLim_rightDerivFun_of_lt_xmin (f : DivFunction) {x : ℝ} (h : x < f.xmin.toReal) :
+    Function.rightLim f.rightDerivFun x = ⊥ := by
+  refine rightLim_eq_of_tendsto (NeBot.ne inferInstance) ?_
+  refine (tendsto_congr' ?_).mpr tendsto_const_nhds
+  filter_upwards [eventually_nhdsWithin_of_eventually_nhds (eventually_lt_nhds h)] with x hx
+  rw [rightDerivFun, if_pos hx]
+
+lemma rightLim_rightDerivFun_of_ge_xmax (f : DivFunction) {x : ℝ} (h : f.xmax ≤ ENNReal.ofReal x) :
+    Function.rightLim f.rightDerivFun x = ⊤ := by
+  refine rightLim_eq_of_tendsto (NeBot.ne inferInstance) ?_
+  refine (tendsto_congr' ?_).mpr tendsto_const_nhds
+  refine eventually_nhdsWithin_of_forall fun y hy ↦ ?_
+  have h' : f.xmax ≤ ENNReal.ofReal y := h.trans <| ENNReal.ofReal_le_ofReal hy.le
+  simp [rightDerivFun, h']
+  refine ENNReal.toReal_le_of_le_ofReal (le_trans ?_ hy.le) (xmin_lt_xmax.le.trans h')
+  suffices 1 ≤ x by linarith
+  rw [← ENNReal.one_le_ofReal]
+  exact one_lt_xmax.le.trans h
 
 -- the `rightLim` matters only at `f.xmin`: `rightDeriv` could be 0 because it has no limit in `ℝ`,
 -- but in that case it should be `⊥`.
 protected noncomputable def rightDerivStieltjes (f : DivFunction) : ERealStieltjes where
-  toFun := fun x ↦
-    if x < f.xmin.toReal then ⊥
-    else if f.xmax ≤ ENNReal.ofReal x then ⊤
-    else Function.rightLim (fun y ↦ (rightDeriv f.realFun y : EReal)) x
-  mono' x y hxy := sorry
-  right_continuous' x := sorry
+  toFun := f.rightDerivFun
+  mono' := f.monotone_rightDerivFun
+  right_continuous' x := by
+    rw [← continuousWithinAt_Ioi_iff_Ici,
+      f.monotone_rightDerivFun.continuousWithinAt_Ioi_iff_rightLim_eq]
+    unfold rightDerivFun
+    split_ifs with h1 h2
+    · exact f.rightLim_rightDerivFun_of_lt_xmin h1
+    · exact f.rightLim_rightDerivFun_of_ge_xmax h2
+    · push_neg at h1 h2
+      have : (fun (x' : ℝ) ↦ if x' < f.xmin.toReal then (⊥ : EReal)
+            else if f.xmax ≤ ENNReal.ofReal x' then ⊤ else
+              Function.rightLim (fun y ↦ ↑(rightDeriv f.realFun y)) x')
+          =ᶠ[𝓝[>] x] fun x' ↦ Function.rightLim (fun y ↦ ↑(rightDeriv f.realFun y)) x' := by
+        by_cases h_top : f.xmax = ∞
+        · refine eventually_nhdsWithin_of_forall fun y hy ↦ ?_
+          simp only
+          rw [if_neg, if_neg]
+          · simp [h_top]
+          · exact not_lt.mpr (h1.trans_lt hy).le
+        have h2' : x < f.xmax.toReal := by
+          rw [← ENNReal.toReal_ofReal (ENNReal.toReal_nonneg.trans h1),
+            ENNReal.toReal_lt_toReal ENNReal.ofReal_ne_top h_top]
+          exact h2
+        filter_upwards [eventually_nhdsWithin_of_eventually_nhds (eventually_lt_nhds h2'),
+          eventually_nhdsWithin_of_forall (fun y hy ↦ h1.trans hy.le)] with y hy1 hy2
+        rw [if_neg (not_lt.mpr hy2), if_neg]
+        rwa [not_le, ENNReal.ofReal_lt_iff_lt_toReal (ENNReal.toReal_nonneg.trans hy2) h_top]
+      obtain ⟨y, h_tendsto⟩ : ∃ y, Tendsto
+          (fun x' ↦ if x' < f.xmin.toReal then (⊥ : EReal)
+            else if f.xmax ≤ ENNReal.ofReal x' then ⊤ else Function.rightLim
+              (fun y ↦ (rightDeriv f.realFun y)) x')
+          (𝓝[>] x) (𝓝 y) := by
+        sorry
+      rw [rightLim_congr (NeBot.ne inferInstance) h_tendsto this]
+      sorry
 
 lemma rightDerivStieltjes_of_lt_xmin {x : ℝ} (hx : x < f.xmin.toReal) :
     f.rightDerivStieltjes x = ⊥ := if_pos hx
 
 lemma rightDerivStieltjes_of_ge_xmax {x : ℝ} (hx : f.xmax ≤ ENNReal.ofReal x) :
     f.rightDerivStieltjes x = ⊤ := by
-  simp only [DivFunction.rightDerivStieltjes]
+  simp only [DivFunction.rightDerivStieltjes, rightDerivFun]
   rw [if_neg, if_pos hx]
   rw [not_lt]
   refine ENNReal.toReal_le_of_le_ofReal ?_ (xmin_lt_xmax.le.trans hx)
@@ -508,14 +586,17 @@ lemma rightDerivStieltjes_of_neg {x : ℝ} (hx : x < 0) :
     f.rightDerivStieltjes x = ⊥ :=
   rightDerivStieltjes_of_lt_xmin (hx.trans_le ENNReal.toReal_nonneg)
 
-lemma rightDerivStieltjes_eq_bot_iff {x : ℝ} :
-    f.rightDerivStieltjes x = ⊥ ↔ x < f.xmin.toReal := by
-  refine ⟨fun h ↦ ?_, fun h ↦ rightDerivStieltjes_of_lt_xmin h⟩
-  sorry
+-- lemma rightDerivStieltjes_eq_bot_iff {x : ℝ} :
+--     f.rightDerivStieltjes x = ⊥ ↔ x < f.xmin.toReal := by
+--   refine ⟨fun h ↦ ?_, fun h ↦ rightDerivStieltjes_of_lt_xmin h⟩
+--   sorry
 
 lemma rightDerivStieltjes_eq_top_iff {x : ℝ} :
     f.rightDerivStieltjes x = ⊤ ↔ f.xmax ≤ ENNReal.ofReal x := by
   refine ⟨fun h ↦ ?_, fun h ↦ rightDerivStieltjes_of_ge_xmax h⟩
+  simp only [DivFunction.rightDerivStieltjes, rightDerivFun] at h
+  split_ifs at h with h1 h2
+  · exact h2
   sorry
 
 lemma rightDerivStieltjes_of_ne_top (hf : ∀ x, 0 < x → f x ≠ ∞) (x : ℝ) :
@@ -557,10 +638,10 @@ lemma rightDerivStieltjes_add :
   simp only [DivFunction.rightDerivStieltjes, xmin_add, toReal_max_xmin, lt_max_iff, xmax_add,
     min_le_iff, not_le.mpr hx_lt_f, not_le.mpr hx_lt_g, or_self, ↓reduceIte]
   by_cases hx_fmin : x < f.xmin.toReal
-  · simp [hx_fmin]
+  · simp [hx_fmin, rightDerivFun]
   by_cases hx_gmin : x < g.xmin.toReal
-  · simp [hx_gmin]
-  simp only [hx_fmin, hx_gmin, or_self, ↓reduceIte]
+  · simp [hx_gmin, rightDerivFun]
+  simp only [hx_fmin, hx_gmin, or_self, ↓reduceIte, rightDerivFun]
   sorry
 
 end RightDeriv
